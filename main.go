354bc6c21936ba9c79f0b37096ed57ba1be612ea
ryota kise <ryota1839@gmail.com>
2020-06-20 15:58:21 +0900

    add: files

--- main.go
+++ main.go
@@ -1,34 +1,15 @@
 package main
 
 import (
+    "./mylib"
     "fmt"
-    "os"
-    "net/smtp"
 )
 
-
 func main() {
+    s := []int{1,2,3,4,5}
+    fmt.Println(mylib.Average(s))
 
-    from := "username@gmail.com"
-    to := "test@gmail.com"
-
-    // func PlainAuth(identity, username, password, host string) Auth
-    auth := smtp.PlainAuth("", from, "password", "smtp.gmail.com")
-
-    msg := []byte("" +
-        "From: 送信した人 <" + from + ">\r\n" +
-        "To: " + to + "\r\n" +
-        "Subject: 件名 subject です\r\n" +
-        "\r\n" +
-        "テスト\r\n" +
-    "")
-
-    // func SendMail(addr string, a Auth, from string, to []string, msg []byte) error
-    err := smtp.SendMail("smtp.gmail.com:587", auth, from, []string{to}, msg)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "エラー: %v\n", err)
-        return
-    }
-
-    fmt.Print("success")
+    mylib.Say()
+    person := mylib.Person{Name: "mike", Age: 20}
+    fmt.Println(person)
 }
