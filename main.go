87a05f73901f1f65d565c2d667bba86ec2d1a967
ryota kise <ryota1839@gmail.com>
2020-06-20 16:21:19 +0900

    add: gin

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "./mylib"
-    "fmt"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-    s := []int{1,2,3,4,5}
-    fmt.Println(mylib.Average(s))
-
-    mylib.Say()
-    person := mylib.Person{Name: "mike", Age: 20}
-    fmt.Println(person)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
 }
