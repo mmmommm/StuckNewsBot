c46fd03525cf2bbf945b99034354958b206f2eb7
ryota kise <ryota1839@gmail.com>
2020-07-10 23:25:57 +0900

    add: scrapping data

---
+++ path/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	doc, err := goquery.NewDocument("https://kabutan.jp/")
+	if err != nil {
+		fmt.Print("url scrapping failed")
+	}
+	res, err := doc.Find(".acrank_top_news1").Html()
+	if err != nil {
+		fmt.Print("dom get failed")
+	}
+	ioutil.WriteFile("path/url.html", []byte(res), os.ModePerm)
+}
