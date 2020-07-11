73f124dc7ef6a8b9563fada42d56efeadb349edb
ryota kise <ryota1839@gmail.com>
2020-07-11 23:15:48 +0900

    fix: 値の取り方

---
+++ scarping/data.go
@@ -0,0 +1,20 @@
+package scaraping
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"fmt"
+)
+
+var href string
+
+func Scaraping() (href string){
+	url := "https://kabutan.jp/"
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		fmt.Print("url scraping failed")
+	}
+	doc.Find(".acrank_top_news1 a").Each(func(_ int, s *goquery.Selection) {
+		href, _ = s.Attr("href")
+	})
+	return href
+}
