2bacf3f46b7c29a3b6b74a65b647d6a6e7f35658
ryota kise <ryota1839@gmail.com>
2020-07-14 20:20:04 +0900

    delete: 不必要なコード

--- main.go
+++ main.go
@@ -104,21 +104,3 @@ func main() {
         log.Fatal(err)
     }
 }
-
-// func search() (links []string){
-//     url := "https://kabutan.jp"
-//     fileInfos, _ := ioutil.ReadFile("./data/index.html")
-//     stringReader := strings.NewReader(string(fileInfos))
-//     doc, err := goquery.NewDocumentFromReader(stringReader)
-//     if err != nil {
-//         log.Fatal(err)
-//     }
-//     doc.Find("table.s_news_list tbody tr td a").Each(func(i int, s *goquery.Selection) {
-//         if i < 11 {
-//             lead, _ := s.Attr("href")
-//             link := (url+ lead)
-//             links = append(links, link)
-//         }
-//     })
-//     return links
-// }
