9c437bf6aa7205178c7f705ed646e5dc9e5edbb8
ryota kise <ryota1839@gmail.com>
2020-07-14 17:39:07 +0900

    fix: スクレイピングをローカルのhtmlファイルからに変更

--- main.go
+++ main.go
@@ -85,7 +85,7 @@ func main() {
                     //newsって打ったら返信でURLが送られてくる
                     case "news":
                         //linkにurlをいれる
-                        links := scraping()
+                        links := search()
                         for _, url := range links {
                             link := string(url)
                             fmt.Print(link)
@@ -119,29 +119,38 @@ func copy() {
 	ioutil.WriteFile("./data/index.html", body, 0666)
 }
 
-func scraping() (links []string) {
-    mainurl := "https://kabutan.jp"
-    url := "https://kabutan.jp/info/accessranking/2_1"
-	doc, err := goquery.NewDocument(url)
-	if err != nil {
-		fmt.Print("url scraping failed")
+// func scraping() (links []string) {
+//     mainurl := "https://kabutan.jp"
+//     url := "https://kabutan.jp/info/accessranking/2_1"
+// 	doc, err := goquery.NewDocument(url)
+// 	if err != nil {
+// 		fmt.Print("url scraping failed")
+//     }
+//     doc.Find("table.s_news_list tbody tr td a").Each(func(i int, s *goquery.Selection) {
+//         if i < 11 {
+//             lead, _ := s.Attr("href")
+//             link := (mainurl+ lead)
+//             links = append(links, link)
+//         }
+//     })
+
+//     return links
+// }
+
+func search() (links []string){
+    url := "https://kabutan.jp"
+    fileInfos, _ := ioutil.ReadFile("./data/index.html")
+    stringReader := strings.NewReader(string(fileInfos))
+    doc, err := goquery.NewDocumentFromReader(stringReader)
+    if err != nil {
+        log.Fatal(err)
     }
     doc.Find("table.s_news_list tbody tr td a").Each(func(i int, s *goquery.Selection) {
         if i < 11 {
             lead, _ := s.Attr("href")
-            link := (mainurl+ lead)
+            link := (url+ lead)
             links = append(links, link)
         }
     })
-
     return links
-}
-
-func search() {
-    fileInfos, _ := ioutil.ReadFile("HTMLのファイルパス")
-    stringReader := strings.NewReader(string(fileInfos))
-    _, err := goquery.NewDocumentFromReader(stringReader)
-    if err != nil {
-        panic(err)
-    }
 }
