9d31ab02b5af27b3de654d40da13636690e64066
ryota kise <ryota1839@gmail.com>
2020-07-16 15:01:55 +0900

    delete: 不要なコード

--- scraping/main.go
+++ scraping/main.go
@@ -10,20 +10,18 @@ import (
 )
 
 func Scraping() (links []string){
-		url := "https://kabutan.jp"
-		fmt.Print(url)
+	url := "https://kabutan.jp"
+	fmt.Print(url)
     fileInfos, _ := ioutil.ReadFile("./data/index.html")
     stringReader := strings.NewReader(string(fileInfos))
     doc, err := goquery.NewDocumentFromReader(stringReader)
     if err != nil {
         log.Fatal(err)
     }
-    doc.Find("table.s_news_list tbody tr td a").Each(func(i int, s *goquery.Selection) {
-        if i < 11 {
-            lead, _ := s.Attr("href")
-            link := (url+ lead)
-            links = append(links, link)
-        }
+doc.Find("table.s_news_list tbody tr td a").Each(func(_ int, s *goquery.Selection) {
+        lead, _ := s.Attr("href")
+        link := (url+ lead)
+        links = append(links, link)
     })
     return links
 }
