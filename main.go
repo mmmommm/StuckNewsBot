53f3e1d9fed62f285a63514db6b824f1de649b77
ryota kise <ryota1839@gmail.com>
2020-08-12 01:19:10 +0900

    fix: 動いたけどurlの形がおかしい

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -24,7 +23,7 @@ type Payload struct {
 
 type Attachment struct {
 	Color     string `json:"color"`
-	TitleLink string `json:"title_link"`
+	Title string `json:"title"`
 }
 
 func main() {
--- repository/scraping.go
+++ repository/scraping.go
@@ -8,24 +8,24 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func Scraping() (links []string) {
-	//urlを踏んだ時のために最初につけるurlを変数として定義
-	url := "https://kabutan.jp"
-	//./data/index.htmlを読み込む
-	fileInfos, _ := ioutil.ReadFile("./data/index.html")
-	stringReader := strings.NewReader(string(fileInfos))
-	//スクレイピングするファイルを指定
-	doc, err := goquery.NewDocumentFromReader(stringReader)
-	if err != nil {
-		log.Fatal(err)
-	}
-	doc.Find("table.s_news_list tbody tr td a").Each(func(_ int, s *goquery.Selection) {
-		//leadに上記のタグのhrefの値を順番に代入
-		lead, _ := s.Attr("href")
-		//リンクを正常に飛ばすためにhttps://kabutan.jpをくっつける
-		link := (url + lead)
-		//[]string型であるlinksに前からlinkを挿入する
-		links = append(links, link)
-	})
-	return links
+func Scraping() (links []string){
+    //urlを踏んだ時のために最初につけるurlを変数として定義
+    url := "https://kabutan.jp"
+    //./data/index.htmlを読み込む
+    fileInfos, _ := ioutil.ReadFile("./data/index.html")
+    stringReader := strings.NewReader(string(fileInfos))
+    //スクレイピングするファイルを指定
+    doc, err := goquery.NewDocumentFromReader(stringReader)
+    if err != nil {
+        log.Fatal(err)
+    }
+    doc.Find("table.s_news_list tbody tr td a").Each(func(_ int, s *goquery.Selection) {
+        //leadに上記のタグのhrefの値を順番に代入
+        lead, _ := s.Attr("href")
+        //リンクを正常に飛ばすためにhttps://kabutan.jpをくっつける
+        link := (url + lead)
+        //[]string型であるlinksに前からlinkを挿入する
+        links = append(links, link)
+    })
+    return links
 }
